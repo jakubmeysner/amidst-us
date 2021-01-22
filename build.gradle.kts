1db24728264bf1473243e64a4bb3d8427b51ebc4
Jakub Meysner <hello@jakubmeysner.me>
2021-01-22 20:02:41 +0100

    Setup project

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+  id("org.jetbrains.kotlin.jvm") version "1.4.20"
+  `java-library`
+}
+
+repositories {
+  jcenter()
+  maven {
+    url = uri("https://hub.spigotmc.org/nexus/content/repositories/public")
+  }
+}
+
+dependencies {
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+  implementation("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+}
---
+++ src/main/kotlin/me/jakubmeysner/amidstus/AmidstUs.kt
@@ -0,0 +1,5 @@
+package me.jakubmeysner.amidstus
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AmidstUs : JavaPlugin()
