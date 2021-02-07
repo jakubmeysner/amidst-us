dc6ee5efdc06f059ea31ad944280e03039d9668d
Jakub Meysner <hello@jakubmeysner.me>
2021-02-07 01:08:57 +0100

    Stop from moving game items in inventory or moving them to off-hand

--- src/main/kotlin/me/jakubmeysner/amidstus/listeners/InventoryListener.kt
+++ src/main/kotlin/me/jakubmeysner/amidstus/listeners/InventoryListener.kt
@@ -5,16 +5,12 @@ import me.jakubmeysner.amidstus.models.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerSwapHandItemsEvent
 
 class InventoryListener(val plugin: AmidstUs) : Listener {
-  @EventHandler
-  fun onPlayerDropItem(event: PlayerDropItemEvent) {
-    if (plugin.games.none { it.players.any { it.bukkitPlayer == event.player } }) return
-    event.isCancelled = true
-  }
-
   @EventHandler
   fun onPlayerInteract(event: PlayerInteractEvent) {
     val game = plugin.games.find { it.players.any { it.bukkitPlayer == event.player } } ?: return
@@ -27,5 +23,23 @@ class InventoryListener(val plugin: AmidstUs) : Listener {
         event.player.performCommand("leavegame")
       }
     }
+  }
+
+  @EventHandler
+  fun onInventoryClick(event: InventoryClickEvent) {
+    if (plugin.games.none { it.players.any { it.bukkitPlayer == event.whoClicked } }) return
+    event.isCancelled = true
+  }
+
+  @EventHandler
+  fun onPlayerDropItem(event: PlayerDropItemEvent) {
+    if (plugin.games.none { it.players.any { it.bukkitPlayer == event.player } }) return
+    event.isCancelled = true
+  }
+
+  @EventHandler
+  fun onPlayerSwapHandItems(event: PlayerSwapHandItemsEvent) {
+    if (plugin.games.none { it.players.any { it.bukkitPlayer == event.player } }) return
+    event.isCancelled = true
   }
 }
