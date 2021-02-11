85c8998eedab010baf343f845f039594227e929c
Jakub Meysner <hello@jakubmeysner.me>
2021-02-11 22:41:33 +0100

    Serialize location to objects instead of strings

--- src/main/kotlin/me/jakubmeysner/amidstus/serializers/LocationSerializer.kt
+++ src/main/kotlin/me/jakubmeysner/amidstus/serializers/LocationSerializer.kt
@@ -1,9 +1,6 @@
 package me.jakubmeysner.amidstus.serializers
 
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import org.bukkit.Bukkit
@@ -11,28 +8,22 @@ import org.bukkit.Location
 import java.util.*
 
 object LocationSerializer : KSerializer<Location> {
-  override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Location", PrimitiveKind.STRING)
+  override val descriptor = LocationSurrogate.serializer().descriptor
 
   override fun serialize(encoder: Encoder, value: Location) {
-    return encoder.encodeString(
-      "${value.world?.uid};${value.x};${value.y};${value.z};${value.yaw};${value.pitch}"
-    )
+    val surrogate = LocationSurrogate(value.world?.uid.toString(), value.x, value.y, value.z, value.yaw, value.pitch)
+    encoder.encodeSerializableValue(LocationSurrogate.serializer(), surrogate)
   }
 
   override fun deserialize(decoder: Decoder): Location {
-    val args = decoder.decodeString().split(";")
-
-    if (args.size != 6) {
-      error("Invalid location")
-    }
-
+    val surrogate = decoder.decodeSerializableValue(LocationSurrogate.serializer())
     return Location(
-      Bukkit.getWorld(UUID.fromString(args[0])),
-      args[1].toDouble(),
-      args[2].toDouble(),
-      args[3].toDouble(),
-      args[4].toFloat(),
-      args[5].toFloat()
+      Bukkit.getWorld(UUID.fromString(surrogate.worldId)),
+      surrogate.x,
+      surrogate.y,
+      surrogate.z,
+      surrogate.yaw,
+      surrogate.pitch
     )
   }
 }
