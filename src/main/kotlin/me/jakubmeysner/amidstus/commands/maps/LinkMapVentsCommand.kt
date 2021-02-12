74a3002ea56008a5d596eb03587169374728b67a
SemiGlassFace <jasiu.piotrowski@gmail.com>
2021-02-12 15:00:01 +0100

    Created RemoveMapVent and LinkMapVents Commands

---
+++ src/main/kotlin/me/jakubmeysner/amidstus/commands/maps/LinkMapVentsCommand.kt
@@ -0,0 +1,70 @@
+package me.jakubmeysner.amidstus.commands.maps
+
+import me.jakubmeysner.amidstus.AmidstUs
+import me.jakubmeysner.amidstus.interfaces.Named
+import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.chat.ComponentBuilder
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+
+class LinkMapVentsCommand(val plugin: AmidstUs) : TabExecutor, Named {
+  override val name = "linkmapvents"
+
+  override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+    if (args.size != 2) {
+      sender.spigot().sendMessage(
+        *ComponentBuilder("Usage: /removemapvent <map name> <vent id>").color(ChatColor.RED).create()
+      )
+    } else {
+      val map = plugin.maps.find { it.name == args[0] }
+
+      if (map == null) {
+        sender.spigot().sendMessage(
+          *ComponentBuilder("Could not find any map with this name!").color(ChatColor.RED).create()
+        )
+      } else {
+        val vent1 = map.vents.find { it.id == args[1] }
+
+        if (vent1 == null) {
+          sender.spigot().sendMessage(
+            *ComponentBuilder("Could not find any vent with this id (first)!").color(ChatColor.RED).create()
+          )
+        } else {
+          val vent2 = map.vents.find { it.id == args[2] }
+
+          if (vent2 == null) {
+            sender.spigot().sendMessage(
+              *ComponentBuilder("Could not find any vent with this id (second)!").color(ChatColor.RED).create()
+            )
+          } else {
+            vent1.linkedVents.add(vent2)
+            vent2.linkedVents.add(vent1)
+
+            sender.spigot().sendMessage(
+              *ComponentBuilder("Succesfully linked vents ${vent1.id} and ${vent2.id} in map ${map.name}.")
+                .color(ChatColor.GREEN).create()
+            )
+          }
+        }
+      }
+    }
+
+    return true
+  }
+
+  override fun onTabComplete(
+    sender: CommandSender,
+    command: Command,
+    alias: String,
+    args: Array<out String>
+  ): List<String> {
+    return when (args.size) {
+      1 -> plugin.maps.map { it.name }.filter { it.startsWith(args[0]) }
+      2 -> plugin.maps.find { it.name == args[0] }?.vents?.map { it.id }?.filter { it.startsWith(args[1]) } ?: listOf()
+      3 -> plugin.maps.find { it.name == args[0] }?.vents?.map { it.id }?.filter { it.startsWith(args[2]) } ?: listOf()
+      else -> listOf()
+    }
+  }
+
+}
---
+++ src/main/kotlin/me/jakubmeysner/amidstus/commands/maps/RemoveMapVentCommand.kt
@@ -0,0 +1,56 @@
+package me.jakubmeysner.amidstus.commands.maps
+
+import me.jakubmeysner.amidstus.AmidstUs
+import me.jakubmeysner.amidstus.interfaces.Named
+import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.chat.ComponentBuilder
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+
+class RemoveMapVentCommand(val plugin: AmidstUs) : TabExecutor, Named {
+  override val name = "removemapvent"
+
+  override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+    if (args.size != 2) {
+      sender.spigot().sendMessage(
+        *ComponentBuilder("Usage: /removemapvent <map name> <vent id>").color(ChatColor.RED).create()
+      )
+    } else {
+      val map = plugin.maps.find { it.name == args[0] }
+
+      if (map == null) {
+        sender.spigot().sendMessage(
+          *ComponentBuilder("Could not find any map with this name!").color(ChatColor.RED).create()
+        )
+      } else {
+        val vent = map.vents.find { it.id == args[1] }
+        if (vent == null) {
+          sender.spigot().sendMessage(
+            *ComponentBuilder("Could not find any vent with this id!").color(ChatColor.RED).create()
+          )
+        } else {
+          map.vents.remove(vent)
+
+          sender.spigot().sendMessage(
+            *ComponentBuilder("Succesfully removed vent ${vent.id} from map ${map.name}.").color(ChatColor.GREEN).create()
+          )
+        }
+      }
+    }
+    return true
+  }
+
+  override fun onTabComplete(
+    sender: CommandSender,
+    command: Command,
+    alias: String,
+    args: Array<out String>
+  ): List<String> {
+    return when (args.size) {
+      1 -> plugin.maps.map { it.name }.filter { it.startsWith(args[0]) }
+      2 -> plugin.maps.find { it.name == args[0] }?.vents?.map { it.id }?.filter { it.startsWith(args[1]) } ?: listOf()
+      else -> listOf()
+    }
+  }
+}
