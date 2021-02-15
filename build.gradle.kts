d6a3c3b3e5431aba8aeb8ea8bd14a9f02ee01735
Jakub Meysner <hello@jakubmeysner.me>
2021-02-15 23:58:19 +0100

    Clean up gradle build settings to improve readability

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,18 @@ version = "1.0.0-SNAPSHOT"
 
 plugins {
   kotlin("jvm") version "1.4.20"
-  kotlin("plugin.serialization") version "1.4.20"
   `java-library`
+  kotlin("plugin.serialization") version "1.4.20"
   id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+dependencies {
+  implementation("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 repositories {
@@ -14,16 +23,11 @@ repositories {
   }
 }
 
-dependencies {
-  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  testImplementation("org.jetbrains.kotlin:kotlin-test")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-  implementation("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-}
-
 tasks {
+  build {
+    dependsOn(shadowJar)
+  }
+
   jar {
     enabled = false
   }
@@ -32,11 +36,7 @@ tasks {
     archiveClassifier.set("")
   }
 
-  build {
-    dependsOn(shadowJar)
+  processResources {
+    expand("version" to version)
   }
-}
-
-tasks.processResources {
-  expand("version" to version)
 }
