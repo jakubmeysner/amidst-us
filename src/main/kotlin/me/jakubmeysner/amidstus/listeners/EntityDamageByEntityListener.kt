f09475e1f5d9fd53228f1a2e5ebac18a54bfba60
Jakub Meysner <hello@jakubmeysner.me>
2021-02-28 14:47:07 +0100

    Add killed players bodies

--- src/main/kotlin/me/jakubmeysner/amidstus/listeners/EntityDamageByEntityListener.kt
+++ src/main/kotlin/me/jakubmeysner/amidstus/listeners/EntityDamageByEntityListener.kt
@@ -1,5 +1,8 @@
 package me.jakubmeysner.amidstus.listeners
 
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.events.PacketContainer
+import com.comphenix.protocol.wrappers.*
 import me.jakubmeysner.amidstus.AmidstUs
 import me.jakubmeysner.amidstus.models.Game
 import me.jakubmeysner.amidstus.models.Player
@@ -8,6 +11,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
+import kotlin.random.Random
 import org.bukkit.entity.Player as BukkitPlayer
 
 class EntityDamageByEntityListener(val plugin: AmidstUs) : Listener {
@@ -33,10 +37,47 @@ class EntityDamageByEntityListener(val plugin: AmidstUs) : Listener {
 
     damagee.dead = true
 
+    val entityId = Random.nextInt()
+
+    val playerInfoData = PlayerInfoData(
+      WrappedGameProfile.fromPlayer(damagee.bukkit),
+      0,
+      EnumWrappers.NativeGameMode.ADVENTURE,
+      WrappedChatComponent.fromText(damagee.bukkit.displayName)
+    )
+
+    val playerInfoPacket = PacketContainer(PacketType.Play.Server.PLAYER_INFO).apply {
+      modifier.writeDefaults()
+      playerInfoDataLists.write(0, listOf(playerInfoData))
+    }
+
+    val namedEntitySpawnPacket = PacketContainer(PacketType.Play.Server.NAMED_ENTITY_SPAWN).apply {
+      integers.write(0, entityId)
+      uuiDs.write(0, playerInfoData.profile.uuid)
+      doubles.write(0, damagee.bukkit.location.x)
+      doubles.write(1, damagee.bukkit.location.y)
+      doubles.write(2, damagee.bukkit.location.z)
+    }
+
+    val entityMetadataPacket = PacketContainer(PacketType.Play.Server.ENTITY_METADATA).apply {
+      integers.write(0, entityId)
+      watchableCollectionModifier.write(0, WrappedDataWatcher.getEntityWatcher(damagee.bukkit).apply {
+        setObject(
+          6,
+          WrappedDataWatcher.Registry.get(EnumWrappers.getEntityPoseClass()),
+          EnumWrappers.EntityPose.SLEEPING.toNms()
+        )
+      }.watchableObjects)
+    }
+
     for (player in game.players) {
       if (!player.dead) {
         player.bukkit.hidePlayer(plugin, damagee.bukkit)
       }
+
+      plugin.protocolManager.sendServerPacket(player.bukkit, playerInfoPacket)
+      plugin.protocolManager.sendServerPacket(player.bukkit, namedEntitySpawnPacket)
+      plugin.protocolManager.sendServerPacket(player.bukkit, entityMetadataPacket)
     }
 
     if (game.players.count { !it.dead && it.impostor } >= game.players.count { !it.dead && !it.impostor }) {
